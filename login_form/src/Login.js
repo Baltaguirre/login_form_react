ac5f0a6472191bbf22334a1f44714ecb52d44abe
baltasar <baltaguirre@gmail.com>
2021-11-16 21:48:51 -0300

    add(src/Login.js): input validation & error message

--- login_form/src/Login.js
+++ login_form/src/Login.js
@@ -1,63 +1,91 @@
 import { useState, useEffect } from 'react'
-
+import {
+  Wrapper,
+  Form,
+  Input,
+  Title,
+  Button,
+  Anchor,
+  Subtitles,
+  Error,
+  P
+} from './Styledcomponents';
 import './App.css';
 
 function Login() {
 
-const [input, setInput] = useState({
-  user:"",
-  password:""
-})
-const [error, setError] = useState('');
+  const pRgx = new RegExp(/^[a-z0-9]+$/i);
+  const [input, setInput] = useState({
+    user: "",
+    password: ""
+  })
+  const [error, setError] = useState('');
 
-const handleChange = (e) => {
-e.preventDefault();
- const { name, value } = e.target;
-setInput({
-  ...input, 
-  [name]: value
-}) 
-};
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    console.log(pRgx.test(value))
+    if (value && !pRgx.test(value)) {
+      setError('“Usuario o contraseña inválidos“')
+    } else {
+      setError('')
+    }
+    setInput({
+      ...input,
+      [name]: value
+    })
+  };
 
 
 
-const handleSubmit = (e) => {
-e.preventDefault();
-alert('form submiteado!')
-setInput({ 
-  user: "",
-  password: ""
-})
-} ; 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(input.user === "" || input.password === ""){
+      setError('Debes ingresar usuario y contraseña')
+    } else {
+      alert('form submiteado!')
+    setInput({
+      user: "",
+      password: ""
+    })
+    }
+    
+  };
 
 
 
 
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-       <div>
-         <input 
-         name="user" 
-         type="text"
-         value={input.user}
-         onChange={handleChange}
-         placeholder="ingrese usuario" 
-         />
-       </div>
-       <div>
-         <input 
-         name="password" 
-         type="password"
-         value={input.password}
-         onChange={handleChange}
-         placeholder="ingrese contraseña" 
-         />
-       </div>
-      </form>
-      <button onClick={handleSubmit}/>
-    </div>
+
+
+    <Wrapper>
+      <Title>PIPE</Title>
+      <Subtitles>
+        <p>Usuario</p>
+        <p>Contraseña</p>
+      </Subtitles>
+      <Form onSubmit={handleSubmit}>
+        { error ? <Error>{error}</Error> : <P>|</P> }
+        <Input
+          name="user"
+          type="text"
+          value={input.user}
+          onChange={handleChange}
+          placeholder="Ingrese usuario"
+        />
+        <Input
+          name="password"
+          type="password"
+          value={input.password}
+          onChange={handleChange}
+          placeholder="Ingrese contraseña"
+        />
+        <Anchor href="localhost:3000">Recuperar contraseña</Anchor>
+        <Button onClick={handleSubmit}>Ingresar</Button>
+      </Form>
+    </Wrapper>
+
   );
 }
 
